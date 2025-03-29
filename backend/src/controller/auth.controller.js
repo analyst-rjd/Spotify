e173f9b6010c9dd8547bef05764c439270211ce2
Raj Das <rajdasmunshi@yahoo.com>
2025-03-30 00:10:36 +0530

    Updated auth.controller

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -7,18 +7,11 @@ export const authCallback = async (req, res, next) => {
     let user = await User.findOne({ clerkId: id });
 
     if (!user) {
-      try {
-        user = await User.create({
-          clerkId: id,
-          fullName: `${firstName} ${lastName}`,
-          imageUrl: imageUrl,
-        });
-      } catch (dbError) {
-        if (dbError.code === 11000) {
-          return next({ status: 400, message: "User already exists" });
-        }
-        return next(dbError); // Pass other errors to middleware
-      }
+      await User.create({
+        clerkId: id,
+        fullName: `${firstName} ${lastName}`,
+        imageUrl: imageUrl,
+      });
     }
 
     res.status(200).json({ success: true, user });
